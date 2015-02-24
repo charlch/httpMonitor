fe5de98b7396d932e6d17001b89bd3b6722829ff
Chris Charles <chris_charles@live.co.uk>
2015-02-24 08:01:34 +0000

    Inital commit of first js file

---
+++ src/httpMonitor.js
@@ -0,0 +1,17 @@
+(function(){
+
+    var module = angular.module("$httpModule",[]);
+
+    module.service("$httpMonitor", ["$http", function($http){
+    
+    return {
+        "onCallStart":
+        "onCallStop":
+    
+    
+    }
+    
+    }]);
+
+
+})();
