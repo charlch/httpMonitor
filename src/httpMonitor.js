2be983c750f48adc3d015e69b4f7d34b9c0e583b
Chris Charles <chris_charles@live.co.uk>
2015-02-25 08:12:25 +0000

    Return number of calls

--- src/httpMonitor.js
+++ src/httpMonitor.js
@@ -1,17 +1,32 @@
 (function(){
 
-    var module = angular.module("$httpModule",[]);
+    var module = angular.module("httpModule",[]);
 
-    module.service("$httpMonitor", ["$http", function($http){
+    module.service("httpMonitor", ["$httpProvider", function($httpProvider){
+        
+        var openCalls = 0;
+        var allCalls = 0;
+        
+        $httpProvider.interceptors.push(function() {
+            return {
+                'request': function(config) {
+                    openCalls++;
+                    allCalls++;
+                    return config;
+                },
+
+                'response': function(response) {
+                    openCalls--;
+                    return response;
+                }
+            };
+        });
+    
     
     return {
-        "onCallStart":
-        "onCallStop":
-    
-    
+        "openCalls": openCalls,
+        "allCalls": allCalls    
     }
     
     }]);
-
-
 })();
